f606a5cdee58069f8fd18cad959d75f5583c6009
nanda9894 <157663872+nanda9894@users.noreply.github.com>
2024-01-25 09:19:41 +0700

    Update Program.cs
    testing 1

--- hithubtest/Program.cs
+++ hithubtest/Program.cs
@@ -12,6 +12,8 @@ namespace hithubtest
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            messagebox.show("hello Nanda");
         }
     }
 }
